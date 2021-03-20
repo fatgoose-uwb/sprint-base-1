3d7e44c899bbadd620d792729dc72c743b25af85
Peng Du <pengdu@Pengs-MacBook-Pro.local>
2021-03-20 14:19:30 -0700

    first commit

---
+++ src/main/java/Problem3.java
@@ -0,0 +1,8 @@
+public class Problem3 {
+
+    // Do not change signature (function name, parameters)
+    public static long SumOfNonUnique(int[] data) {
+        return -1;  // place holder
+        // Add your solution
+    }
+}
---
+++ src/test/java/Problem3Test.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Problem3Test {
+
+    // Example test
+    // Okay to change
+    @Test
+    public void testSumOfNonUnique() {
+        assertTrue(false); // place holder
+        // add your tests here
+    }
+}
